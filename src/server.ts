c1edc5379fb2cfc109bc1d4285f97203750df104
Rafael Negron <r.negron1@umiami.edu>
2022-01-21 21:27:18 -0500

    Small styling update

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import { indexRouter } from "./routes/index";
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
 
 app.use(express.static(path.resolve(__dirname, "public")));
 
