5864686104daa21c181e4bd8fd19935c61dfc4db
Rafael Negron <r.negron1@umiami.edu>
2022-05-15 18:08:39 -0400

    pagination

--- components/Posts/Posts.tsx
+++ components/Posts/Posts.tsx
@@ -1,19 +1,43 @@
-import { FunctionComponent, useContext } from "react";
+import { FunctionComponent, useContext, useEffect, useRef } from "react";
 
 import styles from "./Posts.module.css";
 
+import useIntersection from "../../hooks/useIntersection";
+import { IPost } from "../../interfaces/IPost";
 import BlogContext from "../../store/store";
 import Post from "../Post/Post";
+
+type JSONResponse = {
+  data?: IPost[];
+};
 
 const Posts: FunctionComponent = () => {
   const context = useContext(BlogContext);
 
+  const [containerRef] = useIntersection(context);
+
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+      return;
+    }
+
+    fetch(`/api/posts/${context.page}`)
+      .then((response) => response.json() as JSONResponse)
+      .then(({ data }) => data && context.setPosts(data));
+  }, [context.page]);
+
   return (
-    <div className={styles.posts}>
-      {context.posts.map((post, index) => (
-        <Post key={index} {...post} />
-      ))}
-    </div>
+    <>
+      <div className={styles.posts}>
+        {context.posts.map((post, index) => (
+          <Post key={index} {...post} />
+        ))}
+      </div>
+      <hr className={styles.horizontal} ref={containerRef} />
+    </>
   );
 };
 
